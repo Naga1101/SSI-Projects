f64a82ca00e326a89bd3532922cf166854e2a04b
Rui <ruipedrocerqueira@gmail.com>
2024-03-23 22:50:40 +0000

    messages
    definição dos diferentes tipos de mensagem e envio (sem encript)

--- TPs/TP1/msg_client.py
+++ TPs/TP1/msg_client.py
@@ -3,14 +3,17 @@ import asyncio
 import socket
 
 conn_port = 8443
-max_msg_size = 9999
+max_msg_size = 999
+
 
 class Client:
     """ Classe que implementa a funcionalidade de um CLIENTE. """
+
     def __init__(self, sckt=None):
         """ Construtor da classe. """
         self.sckt = sckt
         self.msg_cnt = 0
+
     def process(self, msg=b""):
         """ Processa uma mensagem (`bytestring`) enviada pelo SERVIDOR.
             Retorna a mensagem a transmitir como resposta (`None` para
@@ -19,18 +22,40 @@ class Client:
         #
         # ALTERAR AQUI COMPORTAMENTO DO CLIENTE
         #
-        print("\n" + msg.decode())
         #print('Received (%d): %r' % (self.msg_cnt , msg.decode()))
+        print("\n" + msg.decode())
         print('Input message to send (empty to finish)')
-        command = input().strip()
 
+        command = input().strip()
 
         if command.startswith('send'):
             print("Enter message body: ")
             message_body = input()
 
-        print(command[0])
-        return command.encode()
+            message_size = len(message_body) + len(command)
+
+            if message_size > max_msg_size:
+                print("Message reached 1000 bytes limit, unable to send")
+            else:
+                # falta caso subject ter mais de 1 palavra
+                # uid vai com o certificado?
+                message = f"{command} {message_body}"
+
+                return message.encode()
+
+        if command.startswith('askqueue'):
+            message = 'askqueue'
+            return message.encode()
+
+        if command.startswith('help'):
+            message = 'help'
+            return message.encode()
+
+        if command.startswith('getmsg'):
+            msg_number = command.split()[1]
+            message = f"getmsg {msg_number}"
+
+            return message.encode()
 
 
 
@@ -48,11 +73,8 @@ async def tcp_echo_client():
     client = Client(addr)
     msg = client.process()
 
-    print(msg)
-
     while msg:
         writer.write(msg)
-        print(msg)
         msg = await reader.read(max_msg_size)
         if msg:
             msg = client.process(msg)
@@ -61,6 +83,7 @@ async def tcp_echo_client():
     writer.write(b'\n')
     print('Socket closed!')
     writer.close()
+
 
 def run_client():
     loop = asyncio.get_event_loop()
--- TPs/TP1/msg_server.py
+++ TPs/TP1/msg_server.py
@@ -22,19 +22,18 @@ class ServerWorker(object):
         self.msg_cnt = 0
 
     def valid_message(self, msg):
-        print(msg)
         key = msg.decode().split(" ")
-        print(key[0])
+        print(key, "valid message key")
         if key[0] in ["askqueue", "help"] and len(key) == 1:
             return 1
         elif key[0] == "-user" and len(key) == 2:
             return 1
         elif key[0] == "getmsg" and len(key) == 2:
             return 1
-        elif key[0] == "send" and len(key) == 3:
+        elif key[0] == "send" and len(key) > 3:
             return 1
-
         return -1
+
     def process(self, msg):
         """ Processa uma mensagem (`bytestring`) enviada pelo CLIENTE.
             Retorna a mensagem a transmitir como resposta (`None` para
@@ -45,7 +44,10 @@ class ServerWorker(object):
         #
         txt = msg.decode()
         print('%d : %r' % (self.id, txt))
-        response = txt.upper().encode()
+        #new_msg = txt.upper().encode()
+
+        #return new_msg if len(new_msg)>0 else None
+        response = "Invalid command or missing arguments! Try help"
 
         if self.valid_message(msg) == 1:
 
@@ -60,23 +62,90 @@ class ServerWorker(object):
             elif txt.startswith("send"):
                 response = self.handle_send_command(txt)
 
+            elif txt.startswith("askqueue"):
+                response = self.handle_askqueue_command(txt)
 
-        else:
-            response = "Invalid command or missing arguments! Try help".encode()
+            elif txt.startswith("getmsg"):
+                response = self.handle_getmsg_command(txt)
 
         return response
 
     def handle_send_command(self, txt):
+        print("Send command received")
+
         tokens = txt.split()
 
         uid = tokens[1]
         subject = tokens[2]
         message = " ".join(tokens[3:])
 
-        message_queue[int(uid)].put((subject, message))
+        print(f"UID: {uid}")
+        print(f"SUBJECT: {subject}")
+        print(f"MESSAGE: {message}")
+
+        # aqui exception ou algum fix caso o uid nao exista
+
+        # False = Ainda nao foi lida
+        message_queue[int(uid)].append((subject, message, False))
+
+        print("Current message queue: ", message_queue)
 
         # Response must be in bytes
         return f"Message queued for UID {uid}".encode()
+
+    def handle_askqueue_command(self, txt):
+        print("askqueue Command Received")
+        # preciso verificar que uid enviou, usar 1 como placeholder
+        response = "<NUM>:<SENDER>:<TIME>:<SUBJECT>\n"
+
+        uid_queue = message_queue[1]
+        message_n = 1
+
+        if not uid_queue:
+            response = "Your inbox is empty"
+        else:
+            for message in uid_queue:
+                # se nao foi lida
+                if message[2] is False:
+                    response += f"{message_n}:<SENDER>:<TIME>:{message[0]}\n"
+                    message_n += 1
+
+        print(response)
+        return response.encode()
+
+    def handle_getmsg_command(self, txt):
+        print("GETMSG Command received")
+
+        uid_placeholder = 1
+
+        tokens = txt.split()
+        print(tokens)
+        print(txt)
+        msg_number = int(tokens[1]) - 1  # fix ao index
+
+        uid_queue = message_queue[uid_placeholder]
+
+        if msg_number < len(uid_queue):
+            message = uid_queue[msg_number]
+            print(f"Message: {message}")
+
+            # Marcar como lido
+            msg_read = (message[0], message[1], True)
+            message_queue[uid_placeholder][msg_number] = msg_read
+
+            print("Message marked as read.")
+            response = (f"Subject: {message[0]}\n"
+                        f"Message: {message[1]}")
+            return response.encode()
+
+        else:
+            print(f"Message number {msg_number + 1} doesnt exist, or its alread marked as read")
+            response = f"Message number {msg_number + 1} doesnt exist, or its alread marked as read"
+
+        return response.encode()
+
+
+
 
     def handle_user_command(self, txt):
 
@@ -84,12 +153,11 @@ class ServerWorker(object):
         return response
 
     def handle_help_command(self, txt):
-        help_text = """
-            • -user <FNAME>
-            • send <UID> <SUBJECT> 
-            • askqueue 
-            • getmsg <NUM>
-            • help
+        help_text = """• -user <FNAME>
+• send <UID> <SUBJECT> 
+• askqueue 
+• getmsg <NUM>
+• help
         """
         response = help_text.encode()
         return response
@@ -99,7 +167,7 @@ def uid_gen(client_address):
     last_uid = list(uids)[-1]
     next_uid = int(last_uid) + 1
     uids[next_uid] = client_address
-    message_queue[next_uid] = Queue()
+    message_queue[next_uid] = []
     return next_uid
 
 
