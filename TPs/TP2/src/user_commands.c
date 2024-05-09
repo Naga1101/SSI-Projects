794e54f8c5c3d7aaaf78287738bee2c23c1e3569
Naga1101 <nunoaguiar2003@gmail.com>
2024-05-09 02:18:18 +0100

    Fix da criação dos folders para os users

--- TPs/TP2/src/user_commands.c
+++ TPs/TP2/src/user_commands.c
@@ -8,21 +8,38 @@
 #include <fcntl.h>
 #include <syslog.h>
 #include <errno.h>
+#include <pwd.h>
 
 #include "../include/struct.h"
 #include "../include/user_commands.h"
 
 void activate_user(char* user, char* folderPath){
-    char userFolderPath[35];
-    snprintf(userFolderPath, sizeof(userFolderPath), "%s/%s", folderPath, user);
-
-    if (mkdir(userFolderPath, 0700) == -1) {
-        syslog(LOG_ERR, "Failed to create the directory: %s e %s\n", strerror(errno), userFolderPath);
+    struct passwd *pwd = getpwnam(user);
+    if (pwd == NULL) {
+        syslog(LOG_ERR, "Failed to retrieve UID for user: %s\n", user);
         exit(EXIT_FAILURE);
     }
 
-    if (execlp("chown", "chown", user, userFolderPath, NULL) == -1) {
+    uid_t uid = pwd->pw_uid;
+
+    // syslog(LOG_NOTICE, "User: %s\n", user);
+    // syslog(LOG_NOTICE, "User converted to uid: %u\n", uid);
+    // syslog(LOG_NOTICE, "Folder path before: %s\n", folderPath);
+
+    char userFolderPath[100];
+    // snprintf(userFolderPath, sizeof(userFolderPath), "%s/%u", folderPath, uid);
+
+    // syslog(LOG_NOTICE, "Folder path after: %s\n", userFolderPath);
+
+    if (mkdir(userFolderPath, 0700) == -1) {
+        syslog(LOG_ERR, "Failed to create the directory: %s and the path is: %s\n", strerror(errno), userFolderPath);
+        exit(EXIT_FAILURE);
+    }
+
+    if (chown(userFolderPath, uid, -1) == -1) {
         syslog(LOG_ERR, "Failed to change ownership of the directory: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    syslog(LOG_NOTICE, "Folder created successfully on path: %s\n", userFolderPath);
 }
